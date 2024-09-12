fa63556ad18840908c70151ac170fa1124e4080c
Martin Boudreau <mboudrea\cstj.qc.ca>
2024-09-12 01:10:09 -0400

    Départ 2024

--- src/App.js
+++ src/App.js
@@ -4,16 +4,11 @@ export default class App {
 	 */
 	static main() {
 		// Récupérer le formulaire
-		var formulaire = document.getElementById("loterie");
+
 		// Ajouter des écouteurs d'événements pour les soumissions
-		formulaire.addEventListener("submit", e => {
-			e.preventDefault();
-			this.calculer();
-		});
+
 		// Ajouter des écouteurs d'événements pour les changements
-		formulaire.addEventListener("change", e => {
-			this.calculer();
-		});
+
 		// Ajouter un écouteur d'événement pour le changement de la liste déroulante
 		formulaire.predefini.addEventListener("change", e => {
 			// Lorsque la liste déroulante change, mettre à jour autres les champs du formulaire
@@ -32,16 +27,10 @@ export default class App {
 	 */
 	static calculer() {
 		// Récupérer les valeurs du formulaire
-		var formulaire = document.getElementById("loterie");
-		var nbBoulesTotal = formulaire.nbBoulesTotal.valueAsNumber;
-		var nbBoulesPigees = formulaire.nbBoulesPigees.valueAsNumber;
 		
 		// Calculer les probabilités
-		var probabilites = this.probabilites(nbBoulesTotal, nbBoulesPigees);
 		
 		// Mettre à jour le contenu HTML
-		var resultat = document.getElementById("resultat");
-		resultat.innerHTML = this.formater(probabilites);
 	}
 	/**
 	 * Calcule la factorielle d'un nombre donné.
@@ -49,26 +38,13 @@ export default class App {
 	 * @param {number} nombre - Le nombre dont on veut calculer la factorielle.
 	 * @returns {number} La factorielle du nombre donné.
 	 */
-    static factorielle(nombre) {
-        var resultat = 1;
-        for (let i = 1; i <= nombre; i += 1) {
-            resultat *= i;
-        }
-        return resultat;
-    }
+
 	/**
 	 * Calcule la factorielle d'un nombre de manière récursive.
 	 * Est donnée à titre d'exemple, mais n'est pas utilisée dans ce programme.
 	 * @param {number} nombre - Le nombre pour lequel calculer la factorielle.
 	 * @returns {number} La factorielle du nombre donné.
 	 */
-    static factorielle_recursif(nombre) {
-		if (nombre <= 1) {
-			return 1;
-		} else {
-			return nombre * this.factorielle_recursif(nombre - 1);
-		}
-	}
 	
 	/**
 	 * Calcule les probabilités en utilisant la formule de la combinaison.
@@ -78,10 +54,7 @@ export default class App {
 	 * @param {number} nbPigees - Le nombre d'éléments tirés.
 	 * @returns {number} - Le résultat de la combinaison.
 	 */
-    static probabilites(nbTotal, nbPigees) {
-        var resultat = this.factorielle(nbTotal) / (this.factorielle(nbPigees) * this.factorielle(nbTotal - nbPigees));
-        return resultat;
-    }
+
 	/**
 	 * Formate un nombre en une chaîne de caractères avec des espaces tous les trois chiffres.
 	 * Exemple: 1234567 devient "1 234 567".
@@ -89,19 +62,19 @@ export default class App {
 	 * @returns {string} Le nombre formaté en chaîne de caractères avec des espaces.
 	 */
     static formater(nombre) {
+		var resultat;
 		// Transformer le nombre en chaîne de caractères sans décimales
-        nombre = nombre.toFixed(0);
+
 		// Chaque partie de trois chiffres sera stockée dans le tableau parties
-        var parties = [];
+
 		// Tant que le nombre a des chiffres
-        while (nombre.length > 0) {
+
 			// Ajouter les trois derniers chiffres au début du tableau parties
-            parties.unshift(nombre.slice(-3));
+
 			// Retirer les trois derniers chiffres du nombre
-            nombre = nombre.slice(0, -3);
-        }
+
 		// Joindre les parties avec un espace
-        var resultat = parties.join(" ");
+
         return resultat;
     }
 }
